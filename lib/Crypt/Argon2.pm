e1e0bf0f3251b835651c9aefd0b7261a3d11d20d
Ed J <mohawk2@users.noreply.github.com>
2016-05-27 22:45:52 +0100

    add VERSION

--- lib/Crypt/Argon2.pm
+++ lib/Crypt/Argon2.pm
@@ -3,6 +3,8 @@ package Crypt::Argon2;
 use strict;
 use warnings;
 
+our $VERSION = '0.001';
+
 use Exporter 5.57 'import';
 our @EXPORT_OK = qw/argon2i_raw argon2d_raw argon2i_pass/;
 use XSLoader;
