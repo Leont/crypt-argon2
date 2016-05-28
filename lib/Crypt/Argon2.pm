d2e8719fbada25c9664166994416c5ba07cb0553
Leon Timmermans <fawaka@gmail.com>
2016-05-28 11:55:51 +0200

    Add really slow test, conditionally

--- lib/Crypt/Argon2.pm
+++ lib/Crypt/Argon2.pm
@@ -6,7 +6,7 @@ use warnings;
 use Exporter 5.57 'import';
 our @EXPORT_OK = qw/argon2i_raw argon2d_raw argon2i_pass/;
 use XSLoader;
-XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
+XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION // 0);
 
 1;
 
