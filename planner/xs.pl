05374bc9e162e3d336d7d4a68cab0606a20a452c
Leon Timmermans <fawaka@gmail.com>
2024-07-23 20:34:18 +0200

    Try compiling opt.c even without arch flags

--- planner/xs.pl
+++ planner/xs.pl
@@ -6,21 +6,20 @@ use File::Temp 'tempfile';
 my (@compiler_flags, @linker_flags);
 my $impl = 'ref';
 
-if ($ENV{PERL5_CPAN_IS_RUNNING}) {
-	my $checker = ExtUtils::Builder::Planner->new;
-	$checker->load_module('ExtUtils::Builder::AutoDetect::C');
+my $checker = ExtUtils::Builder::Planner->new;
+$checker->load_module('ExtUtils::Builder::AutoDetect::C', undef, config => config());
 
-	my $arch = $ENV{CRYPT_ARGON2_ARCH} || 'native';
-	my @flags = $arch eq 'NONE' ? () : ( "-march=$arch" );
+my $default_arch = $ENV{PERL5_CPAN_IS_RUNNING} ? 'native' : 'NONE';
+my $arch = $ENV{CRYPT_ARGON2_ARCH} || $default_arch;
+my @flags = $arch eq 'NONE' ? () : ( "-march=$arch" );
 
-	my (undef, $target) = tempfile('compile_checkXXXX', TMPDIR => 1, UNLINK => 1, SUFFIX => '.o', OPEN => 0);
+my (undef, $target) = tempfile('compile_checkXXXX', TMPDIR => 1, UNLINK => 1, SUFFIX => '.o', OPEN => 0);
 
-	$checker->compile('src/opt.c', $target, include_dirs => [ 'include' ], extra_args => \@flags);
-	if (eval { $checker->materialize->run($target); 1 }) {
-		$impl = 'opt';
-		@compiler_flags = @flags;
-	};
-};
+$checker->compile('src/opt.c', $target, include_dirs => [ 'include' ], extra_args => \@flags);
+if (eval { $checker->materialize->run($target); 1 }) {
+	$impl = 'opt';
+	@compiler_flags = @flags;
+}
 
 if ($^O ne 'MSWin32') {
 	unshift @compiler_flags, '-pthread';
